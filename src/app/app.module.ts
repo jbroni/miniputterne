ddebb21b4b2f497f10a96b68cd5f71d41339292c
Jesper Broni Andersen <jba@ccieurope.com>
2018-08-01 18:21:29 +0200

    Setup firestore connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { firebase } from '../environments/firebase';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
