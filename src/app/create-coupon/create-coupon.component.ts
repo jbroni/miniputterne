6b5059ef80e01e857bcc3b6c80ba6f5eb7341ccc
Jesper Broni Andersen <jba@ccieurope.com>
2018-08-03 17:44:13 +0200

    Navigate to system page if create page was refreshed

--- src/app/create-coupon/create-coupon.component.ts
+++ src/app/create-coupon/create-coupon.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NewCouponService } from '../new-coupon.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create-coupon.component.html',
   styleUrls: ['./create-coupon.component.css']
 })
-export class CreateCouponComponent {
-  constructor(private newCouponService: NewCouponService) {}
+export class CreateCouponComponent implements OnInit {
+  constructor(private newCouponService: NewCouponService, private router: Router) {}
+
+  ngOnInit() {
+    if (!this.week) {
+      // Page was reloaded while on the create page and the week was lost.
+      this.router.navigate(['/system']);
+    }
+  }
 
   public get week(): number {
     return this.newCouponService.newCouponWeek;
